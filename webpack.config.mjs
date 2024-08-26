411fddc657a5ed45a51371a192bff1c9fd0a15b7
Ihar Sobaleu <ihar.sobaleu@rsgroup.com>
2024-08-26 12:02:25 +0100

    feat: add bin banner via webpack plugin

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -3,6 +3,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import nodeExternals from 'webpack-node-externals';
+import webpack from 'webpack';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
@@ -13,6 +14,9 @@ export default {
     index: './src/index.ts',
   },
   externals: [nodeExternals()],
+  plugins: [
+    new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+  ],
   output: {
     filename: 'index.cjs',
     path: path.resolve(__dirname, 'lib'),
